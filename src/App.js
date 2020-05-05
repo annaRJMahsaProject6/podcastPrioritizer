1d04747405dd1d03cbc9dbc387a4b6f5dcefa960
anna liang <annajliang@gmail.com>
2020-05-05 17:48:32 -0400

    finish landing page and get listenNotes api to work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Header from './components/Header';
+import Podcast from './components/Podcast'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>TESTING</h1>
-      </div>
+render() {
+  return (
+    <div className='App'>
+      <Header />
+      <Podcast />
+     </div>
     );
   }
 }
