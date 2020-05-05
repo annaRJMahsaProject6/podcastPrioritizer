1631610b1c83fe2dbb8beb272b0db2801c5f4701
Ranjodh Singh <ranjodhsingh24@gmail.com>
2020-05-05 16:58:50 -0400

    search ahead and static map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './App.css';
+import SearchAhead from './SearchAhead';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>TESTING</h1>
+      <div className="App wrapper">
+        <h1>TESTING</h1>
+        <SearchAhead />
       </div>
     );
   }
