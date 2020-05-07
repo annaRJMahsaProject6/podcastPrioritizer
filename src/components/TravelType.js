fae3630867ef3c2db83adca1d410dfa72a64bb32
mahsa Ashtari <mahsa_91@live.com>
2020-05-07 16:48:12 -0400

    fixed travelType button click

--- src/components/TravelType.js
+++ src/components/TravelType.js
@@ -2,17 +2,24 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function TravelType (props){
+    let handleClick=(event)=>{
+        event.persist()
+        const id=event.target.id || event.target.parentElement.id || event.target.parentElement.parentElement.id
+        props.chooseTravelType(id)
+}
     return(
-        <section>
+        <section className="travelTypeSelection">
+            <div className="wrapper">
             <h2>please indicate your prefered form of travel</h2>
-            <button id="walk" onClick={(event)=>{props.chooseTravelType(event.target.id)}}>
-                <FontAwesomeIcon icon="walking" className="walkingIcon"></FontAwesomeIcon>
-                <p>{`your walking travel time is ${props.walkTime} hours`}</p>
+            <button id="walk" onClick={handleClick} className="option">
+                <FontAwesomeIcon icon="walking" className="icon"></FontAwesomeIcon>
+                <p >{`your walking travel time is ${props.walkTime} hours`}</p>
             </button>
-            <button id="cycle" onClick={(event)=>{props.chooseTravelType(event.target.id)}}>
-                <FontAwesomeIcon icon="biking" className="walkingIcon"></FontAwesomeIcon>
+            <button id="cycle" onClick={handleClick}>
+                <FontAwesomeIcon icon="biking" className="icon"></FontAwesomeIcon>
                 <p>{`your cycling travel time is ${props.cycleTime}`}</p>
             </button>
+            </div>
         </section>
     )
 }
