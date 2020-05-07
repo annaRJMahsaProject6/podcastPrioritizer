8925a059da0a37a3b8e21e145f77c6ab231efdd1
anna liang <annajliang@gmail.com>
2020-05-07 19:32:39 -0400

    style forms, travelType buttons, etc

--- src/components/TravelType.js
+++ src/components/TravelType.js
@@ -7,20 +7,32 @@ function TravelType (props){
         const id=event.target.id || event.target.parentElement.id || event.target.parentElement.parentElement.id
         props.chooseTravelType(id)
 }
-    return(
-        <section className="travelTypeSelection">
-            <div className="wrapper">
-            <h2>please indicate your prefered form of travel</h2>
+    return (
+      <section className="travelTypeSection">
+        <div className="travelTypeSelection wrapper">
+          <h2 className="travelHeading">Travel Preference?</h2>
+          <p class="safetyWarning">
+            <span className="warning">Warning:</span> Biking with headphones on
+            is <span className="emphasisText">not</span> suggested.
+          </p>
+          <p class="safetyWarning">
+            Please be safe when listening to audio while cycling.
+          </p>
+          <div className="travelTypeButtonContainer">
             <button id="walk" onClick={handleClick} className="option">
-                <FontAwesomeIcon icon="walking" className="icon"></FontAwesomeIcon>
-                <p >{`your walking travel time is ${props.walkTime} hours`}</p>
+              <FontAwesomeIcon
+                icon="walking"
+                className="icon"
+              ></FontAwesomeIcon>
+              <p>{`your walking travel time is ${props.walkTime} hour(s)`}</p>
             </button>
-            <button id="cycle" onClick={handleClick}>
-                <FontAwesomeIcon icon="biking" className="icon"></FontAwesomeIcon>
-                <p>{`your cycling travel time is ${props.cycleTime}`}</p>
+            <button id="cycle" onClick={handleClick} className="option">
+              <FontAwesomeIcon icon="biking" className="icon"></FontAwesomeIcon>
+              <p>{`your cycling travel time is ${props.cycleTime} hour(s)`}</p>
             </button>
-            </div>
-        </section>
-    )
+          </div>
+        </div>
+      </section>
+    );
 }
 export default TravelType
