a913ffad59231df36eac7b361994e1d6a0c0d7e0
mahsa Ashtari <mahsa_91@live.com>
2020-05-07 19:57:27 -0400

    minor changes

--- src/components/TravelType.js
+++ src/components/TravelType.js
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function TravelType (props){
-    let handleClick=(event)=>{
+class TravelType extends Component{
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+     handleClick=(event)=>{
         event.persist()
         const id=event.target.id || event.target.parentElement.id || event.target.parentElement.parentElement.id
         props.chooseTravelType(id)
 }
+    render(){
     return(
         <section className="travelTypeSelection">
             <div className="wrapper">
             <h2>please indicate your prefered form of travel</h2>
-            <button id="walk" onClick={handleClick} className="option">
+            <button id="walk" onClick={this.handleClick}>
                 <FontAwesomeIcon icon="walking" className="icon"></FontAwesomeIcon>
-                <p >{`your walking travel time is ${props.walkTime} hours`}</p>
+                <p >{`your walking travel time is ${this.props.walkTime} hours`}</p>
             </button>
-            <button id="cycle" onClick={handleClick}>
+            <button id="cycle" onClick={this.handleClick}>
                 <FontAwesomeIcon icon="biking" className="icon"></FontAwesomeIcon>
-                <p>{`your cycling travel time is ${props.cycleTime}`}</p>
+                <p>{`your cycling travel time is ${this.props.cycleTime}`}</p>
             </button>
             </div>
         </section>
-    )
+    )}
 }
 export default TravelType
