a13061082b3c09e05b55f0b34246cb6703a169c7
anna liang <annajliang@gmail.com>
2020-05-07 01:39:08 -0400

    collapse any podcast text over 250 words in length

--- src/components/PodcastDisplay.js
+++ src/components/PodcastDisplay.js
@@ -1,4 +1,5 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
+import ReadMoreAndLess from "react-read-more-less";
 
 class PodcastDisplay extends Component {
   constructor() {
@@ -11,6 +12,9 @@ class PodcastDisplay extends Component {
   // thank you Salvatore https://stackoverflow.com/questions/39779527/toggle-play-pause-in-react-with-audio
 
   audio = new Audio();
+
+  showMore = () => this.setState({ showAll: true });
+  showLess = () => this.setState({ showAll: false });
 
   playAudio = (audio) => {
     console.log(typeof audio);
@@ -31,7 +35,7 @@ class PodcastDisplay extends Component {
   };
 
   render() {
-      console.log(this.props.podcastList);
+    console.log(this.props.podcastList);
     return (
       <section className="podcastContainer">
         <h2 className="podcastHeader">Pick Your Podcast</h2>
@@ -52,14 +56,19 @@ class PodcastDisplay extends Component {
                     alt=""
                     className="podcastImg"
                   />
-                  <button>Listen</button>
+                  <button className="audioButton">Listen</button>
                 </div>
                 <div className="podcastInfo">
                   <h3>{podcast.title_original}</h3>
-                  <div className="podcastDescription">
-                    <p>{podcast.description_original}</p>
-                  </div>
-                  <p>
+                  <ReadMoreAndLess
+                    ref={this.Readmore}
+                    charLimit={250}
+                    readMoreText="Read More"
+                    readLessText="&nbsp;Read Less"
+                  >
+                    {podcast.description_original}
+                  </ReadMoreAndLess>
+                  <p className="podcastLength">
                     Length: {Math.floor(podcast.audio_length_sec / 60)} minutes
                   </p>
                 </div>
