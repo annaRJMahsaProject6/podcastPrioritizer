e2796f86fbcae5cfbf19b48587e8f47d6714de1d
Ranjodh Singh <ranjodhsingh24@gmail.com>
2020-05-08 15:05:03 -0400

    Updates

---
+++ precache-manifest.967929fc6e0565dc6c49b2f8b44cc666.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3d661603e40e29619e5096fcd81e0f",
+    "url": "/podcastPrioritizer/index.html"
+  },
+  {
+    "revision": "b6662c2a4b9da0b03fb1",
+    "url": "/podcastPrioritizer/static/css/main.a4eaa83b.chunk.css"
+  },
+  {
+    "revision": "d694b8f61bb2fec45bf3",
+    "url": "/podcastPrioritizer/static/js/2.fd28dbd6.chunk.js"
+  },
+  {
+    "revision": "318df082d58536ffea45fe4c101b795e",
+    "url": "/podcastPrioritizer/static/js/2.fd28dbd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6662c2a4b9da0b03fb1",
+    "url": "/podcastPrioritizer/static/js/main.e593d8f7.chunk.js"
+  },
+  {
+    "revision": "9fe28680c0ee5127b048",
+    "url": "/podcastPrioritizer/static/js/runtime-main.77d81851.js"
+  },
+  {
+    "revision": "1e18c276d36ae20e4ac7127d42ff945d",
+    "url": "/podcastPrioritizer/static/media/bg3.1e18c276.jpg"
+  }
+]);
